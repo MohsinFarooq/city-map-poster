31b70874dde71a83f9655198cf74909bb9fcb3c6
mohsin <mohsin@pragtex.com>
2026-03-04 20:10:39 +0530

    allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+  allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
